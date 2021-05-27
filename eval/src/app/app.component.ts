36b45e5b7fea8a2b7d33588b0109424eead9e79f
Tranquility42 <neffetimahmoud.dev@gmail.com>
2021-05-27 15:44:42 +0200

    Exercice4

--- eval/src/app/app.component.ts
+++ eval/src/app/app.component.ts
@@ -8,7 +8,8 @@ import { Statistique } from './models/statistique';
 })
 export class AppComponent {
   title = 'eval';
-  public statistiqueLoire : Statistique = new Statistique("fa1f5f40-be3b-11eb-91ec7f5875ecfb46","Démographie Loire","150k" , "SUCCESS");
-  public statistiqueRhone : Statistique = new Statistique("fa1f5f40-be3b-11eb-91ec7f5875ecfb45","Démographie Rhone","350k" , "SUCCESS");
-
+  public statTab: Statistique[] = [
+    new Statistique("fa1f5f40-be3b-11eb-91ec7f5875ecfb46","Démographie Loire","150k" , "SUCCESS"),
+    new Statistique("fa1f5f40-be3b-11eb-91ec7f5875ecfb45","Démographie Rhone","350k" , "SUCCESS"),
+     ];
 }
