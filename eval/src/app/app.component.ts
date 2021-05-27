15321100882637c7ed93bd4b354fc04f2c5385ae
Tranquility42 <neffetimahmoud.dev@gmail.com>
2021-05-27 16:47:23 +0200

    exercice5

--- eval/src/app/app.component.ts
+++ eval/src/app/app.component.ts
@@ -11,5 +11,5 @@ export class AppComponent {
   public statTab: Statistique[] = [
     new Statistique("fa1f5f40-be3b-11eb-91ec7f5875ecfb46","Démographie Loire","150k" , "SUCCESS"),
     new Statistique("fa1f5f40-be3b-11eb-91ec7f5875ecfb45","Démographie Rhone","350k" , "SUCCESS"),
-     ];
+    ];
 }
